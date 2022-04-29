be352c90b88080450e29aeb35a61669f4f3be2fd
JurgensM <jurgensmarpali2@icloud.com>
2022-04-29 17:07:06 +0200

    Estate Page

---
+++ slider.js
@@ -0,0 +1,18 @@
+// var mySwiper = new Swiper('.mioSwiper', {
+    
+//     loop: true,
+//     speed: 1000,
+//     slidesPerView: 1,
+//     navigation: {
+//        nextEl: '.swiper-button-next',
+//        prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//        el: '.swiper-pagination',
+//        type: 'bullets',
+//        clickable: true,
+//     },
+  
+   
+  
+//   });
