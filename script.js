37f20be3655c5cb0c2c688a46afa17a7f3ad4998
JurgensM <jurgensmarpali2@icloud.com>
2022-05-04 01:10:44 +0200

    Concierge Page Done

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+// preloader
 $(document).ready(function() {
-  
   setTimeout(function() {
     $('#ctn-preloader').addClass('loaded');
-    // al termine viene visualizzato lo scroll
+    // scroll visualized at the end
     $('body').removeClass('no-scroll-y');
 
     if ($('#ctn-preloader').hasClass('loaded')) {
-      // intera sezione del preloader viene eliminata 
+      // whole preloader section is deleted
       $('#preloader').delay(1000).queue(function() {
         $(this).remove();
       });
     }
   }, 1000);
-  
 });
 
 // change logo color when the navbar is black
 window.addEventListener("scroll", (event) => {
   let scrollY = this.scrollY;
-
+  
   if (scrollY >= 100){
     $("#logo").css({
       "display": "none",
@@ -36,10 +35,7 @@ window.addEventListener("scroll", (event) => {
     }
 });
 
-
-
-
-
+// transition of the hamburger button
 function myFunction(x) {
   x.classList.toggle("change");
 }
@@ -53,44 +49,108 @@ btn.addEventListener('click', function () {
   }
 }, false);
 
+// navbar background is black if scroll is > 100
 $(window).scroll(function() {    
   var scroll = $(window).scrollTop();
 
   if (scroll >= 100) {
-      $(".container").addClass("scrolling");
+    $(".container").addClass("scrolling");
   } else {
       $(".container").removeClass("scrolling");
+    }
+});
+
+//slider
+$(function() {
+
+  // slider type
+  $t = "slide"; // opitions are fade and slide
+  
+  //variables
+  $f = 1000,  // fade in/out speed
+  $s = 1000,  // slide transition speed (for sliding carousel)
+  $d = 5000;  // duration per slide
+  
+  $n = $('.slide').length; //number of slides
+  $w = $('.slide').width(); // slide width
+  $c = $('.slider').width(); // container width
+   $ss = $n * $w; // slideshow width
+
+  
+    function timer() {
+      $('.timer').animate({"width":$w}, $d);
+      $('.timer').animate({"width":0}, 0);
+  }
+
+
+// fading function
+  function fadeInOut() {
+    timer();
+      $i = 0;    
+      var setCSS = {
+          'position' : 'absolute',
+          'top' : '0',
+          'left' : '0'
+      }        
+      
+      $('.slide').css(setCSS);
+      
+      //show first item
+      $('.slide').eq($i).show();
+      
+
+      setInterval(function() {
+        timer();
+          $('.slide').eq($i).fadeOut($f);
+          if ($i == $n - 1) {
+              $i = 0;
+          } else {
+              $i++;
+          }
+          $('.slide').eq($i).fadeIn($f, function() {
+              $('.timer').css({'width' : '0'});
+          });
+
+      }, $d);
+      
+  }
+  
+  function slide() {
+    timer();
+      var setSlideCSS = {
+          'float' : 'left',
+          'display' : 'inline-block',
+          'width' : $c
+      }
+      var setSlideShowCSS = {
+          'width' : $ss // set width of slideshow container
+      }
+      $('.slide').css(setSlideCSS);
+      $('.slideshow').css(setSlideShowCSS); 
+      
+      
+      setInterval(function() {
+          timer();
+          $('.slideshow').animate({"left": -$w}, $s, function(){
+              // to create infinite loop
+              $('.slideshow').css('left',0).append( $('.slide:first'));
+          });
+      }, $d);
+      
+  }
+  
+  if ($t == "fade") {
+      fadeInOut();
+      
+  } if ($t == "slide") {
+      slide();
+      
+  } else {
+    
   }
 });
 
-var swiper = new Swiper(".mySwiper", {
-  observer: true,
-            observeParents: true,
-            loop: true,
-            spaceBetween: 10,
-            slidesPerView: 4,
-            freeMode: true,
-            watchSlidesVisibility: true,
-            watchSlidesProgress: true,
-  autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-  },
-loop: true,
-
-direction: "vertical",
-
-effect: 'cube',
-
-pagination: {
-  el: ".swiper-pagination",
-  clickable: true,
-},
-
-navigation: {
-    nextEl: 'swiper-button-next',
-    prevEl: '.swiper-button-prev',
-},
-
-});
-
+// year counter in footer 2
+const year = document.getElementById("actualYear");
+const actualYear = new Date();
+year.innerHTML = actualYear.getFullYear();
